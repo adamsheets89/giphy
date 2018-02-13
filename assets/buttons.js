361a44254ce6b5ce19a6f7c54e307e09e7f237d7
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-12 18:16:54 -0600

    buttons call giphy but loses functionality when new buttons added

--- assets/buttons.js
+++ assets/buttons.js
@@ -2,33 +2,34 @@ var teams = ["Bulls", "Lakers", "Celtics", "Heat", "Pistons", "Cavaliers", "76er
 
 function alertTeamName() {
     var teamName = $(this).attr("data-name");
-    alert(teamName)
-  }
+
+
+}
 
 function createButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise we will have repeat buttons)
+    
     $("#team-buttons").empty();
 
-    // Looping through the array of movies
+    // Looping through the array of teams
+    
     for (var i = 0; i < teams.length; i++) {
+        
 
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
         var a = $("<button>");
-        // Adding a class of movie to our button
+
         a.addClass("team");
         // Adding a data-attribute
         a.attr("data-name", teams[i]);
-        // Providing the initial button text
+      
         a.text(teams[i]);
         // Adding the button to the HTML
-        $("#team-buttons").append(a);
+        $("#team-buttons").append(a); 
+        
     }
 }
 
-$("#addTeam").on("click", function(event) {
+$("#addTeam").on("click", function (event) {
     // Preventing the buttons default behavior when clicked (which is submitting a form)
     event.preventDefault();
     // This line grabs the input from the textbox
@@ -40,9 +41,40 @@ $("#addTeam").on("click", function(event) {
     // Calling renderButtons which handles the processing of our movie array
     createButtons();
 
-  });
+});
 
-  $(document).on("click", ".team", alertTeamName);
+$(document).on("click", ".team", alertTeamName);
 
-  // Calling the renderButtons function to display the intial buttons
-  createButtons();
+
+createButtons();
+
+$(".team").on("click", function (event) {
+
+    event.preventDefault();
+
+    var team = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + team + "&apikey=6CM6VNSiKBTp6aeVJFDDWXxJvphW4P3b";
+    
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var results = response.data
+        
+
+        for (var j = 0; j < results.length; j++) {
+            var teamsDiv = $("<div class='item'>");
+            var rating = results[j].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var teamsImage = $("<img>");
+            teamsImage.attr("src", results[j].images.fixed_height.url);
+            teamsDiv.prepend(p);
+            teamsDiv.prepend(teamsImage);
+            $("#gifs-appear-here").prepend(teamsDiv);
+           
+        }
+    });
+});
+
