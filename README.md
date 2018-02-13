c072a46202af615eaad27e4f63d254d2107ad333
Adam Sheets <adamsheets@Adams-MacBook-Pro.local>
2018-02-12 21:11:07 -0600

    additional CSS is all that remains

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # giphy
+This is my first application involving the use of AJAX to implement API's, therby creating a dynamic page for the viewer. 
+Upon loading, the app displays nine buttons, each of which is labeled with a different NBA team.
+By utilizing the "input box", a user is able to add any additional team (although any term, while unrelated to my theme, may be added), which is then added to the list of buttons.
+By clicking any of the buttons on the page, either those provided upon the launching of the application or those added by the user, the page then displays ten giphys associated with the button name.
+Upon dislay, each of these giphys is static. By clicking on any of the images, the viewer then seens the complete giphy on a loop, and is able to pause the giphy simply be reclicking.
+
+To create this application, I made use of HTML, CSS, Javascript, jQuery, and AJAX.
--- assets/buttons.js
+++ assets/buttons.js
@@ -1,32 +1,30 @@
+//variables for buttons on page
 var teams = ["Bulls", "Lakers", "Celtics", "Heat", "Pistons", "Cavaliers", "76ers", "Knicks", "Warriors"];
 
 function alertTeamName() {
     var teamName = $(this).attr("data-name");
 
-
 }
 
 function createButtons() {
 
-    
     $("#team-buttons").empty();
 
     // Looping through the array of teams
-    
+
     for (var i = 0; i < teams.length; i++) {
-        
 
         var a = $("<button>");
 
         a.addClass("team");
         // Adding a data-attribute
         a.attr("data-name", teams[i]);
-      
+        // a.attr("data-state", "still");
         a.text(teams[i]);
         // Adding the button to the HTML
-        $("#team-buttons").append(a); 
-        
+        $("#team-buttons").append(a);
     }
+    addGiphys()
 }
 
 $("#addTeam").on("click", function (event) {
@@ -37,44 +35,61 @@ $("#addTeam").on("click", function (event) {
 
     // Adding the movie from the textbox to our array
     teams.push(team);
-
+    $("#choice-input").val()
     // Calling renderButtons which handles the processing of our movie array
     createButtons();
 
 });
 
 $(document).on("click", ".team", alertTeamName);
-
+$(document).on("click", ".bballGiphs", controlGiphs);
 
 createButtons();
 
-$(".team").on("click", function (event) {
+function addGiphys() {
 
-    event.preventDefault();
+    $(".team").on("click", function (event) {
 
-    var team = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + team + "&apikey=6CM6VNSiKBTp6aeVJFDDWXxJvphW4P3b";
+        event.preventDefault();
+
+        var team = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + team + "&apikey=6CM6VNSiKBTp6aeVJFDDWXxJvphW4P3b&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data
     
 
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var results = response.data
-        
-
-        for (var j = 0; j < results.length; j++) {
-            var teamsDiv = $("<div class='item'>");
-            var rating = results[j].rating;
-            var p = $("<p>").text("Rating: " + rating);
-            var teamsImage = $("<img>");
-            teamsImage.attr("src", results[j].images.fixed_height.url);
-            teamsDiv.prepend(p);
-            teamsDiv.prepend(teamsImage);
-            $("#gifs-appear-here").prepend(teamsDiv);
-           
-        }
+            for (var j = 0; j < results.length; j++) {
+                var teamsDiv = $("<div class='item'>");
+                var rating = results[j].rating;
+                var p = $("<p>").text("Rating: " + rating);
+                var teamsImage = $("<img>");
+                var animate = results[j].images.fixed_height.url;
+                var static = results[j].images.fixed_height_still.url;
+                teamsImage.addClass("bballGiphs");
+                teamsImage.attr("data-animate", animate);
+                teamsImage.attr("src", static);
+                teamsImage.attr("data-still", static);
+                teamsImage.attr("data-state", "still");
+                teamsDiv.prepend(p);
+                teamsDiv.prepend(teamsImage);
+                $("#gifs-appear-here").prepend(teamsDiv);
+            }
+        });
     });
-});
-
+}
+    
+function controlGiphs() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "data-animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "data-still");
+    }
+    console.log("this: " + this);
+};
